ae9513a4d4e049861d21a743f68c68b108415e6b
bulatko <buloch03@yandex.ru>
2019-11-02 12:30:07 +0300

    release 0.2

--- updater.php
+++ updater.php
@@ -1,10 +1,11 @@
 <?php
 
 require 'bd.php';
+require 'utils.php';
 
 $q = $mysqli->query("select wallet from wallets");
 
 while ($row = mysqli_fetch_array($q)){
     $wallet = $row[0];
-
+    get_content("$URL/worker.php?wallet=$wallet");
 }
